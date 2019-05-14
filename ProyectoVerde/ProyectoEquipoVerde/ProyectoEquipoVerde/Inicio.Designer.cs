aa1f0e1e996f4135cb630e78de5609bc370c7248
ALUMNADO\catama02 <yoen2minutos2@gmail.com>
2019-05-14 20:12:45 +0200

    Añado Interfaz (Login/Register)

---
+++ ProyectoVerde/ProyectoEquipoVerde/ProyectoEquipoVerde/Inicio.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ProyectoEquipoVerde
+{
+    partial class INICIO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(INICIO));
+            this.ptrLogo = new System.Windows.Forms.PictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContrasenya = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasenya = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptrLogo
+            // 
+            this.ptrLogo.Image = ((System.Drawing.Image)(resources.GetObject("ptrLogo.Image")));
+            this.ptrLogo.Location = new System.Drawing.Point(133, 12);
+            this.ptrLogo.Name = "ptrLogo";
+            this.ptrLogo.Size = new System.Drawing.Size(177, 155);
+            this.ptrLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptrLogo.TabIndex = 0;
+            this.ptrLogo.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(51, 203);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblContrasenya
+            // 
+            this.lblContrasenya.AutoSize = true;
+            this.lblContrasenya.Location = new System.Drawing.Point(33, 236);
+            this.lblContrasenya.Name = "lblContrasenya";
+            this.lblContrasenya.Size = new System.Drawing.Size(64, 13);
+            this.lblContrasenya.TabIndex = 2;
+            this.lblContrasenya.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(103, 200);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(161, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtContrasenya
+            // 
+            this.txtContrasenya.Location = new System.Drawing.Point(103, 233);
+            this.txtContrasenya.Name = "txtContrasenya";
+            this.txtContrasenya.Size = new System.Drawing.Size(161, 20);
+            this.txtContrasenya.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(83, 282);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(83, 328);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "REGISTER";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(164, 121);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(335, 308);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // INICIO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(189)))), ((int)(((byte)(89)))));
+            this.ClientSize = new System.Drawing.Size(449, 367);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtContrasenya);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContrasenya);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.ptrLogo);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "INICIO";
+            this.Text = "Inicio";
+            ((System.ComponentModel.ISupportInitialize)(this.ptrLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptrLogo;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContrasenya;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasenya;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ ProyectoVerde/ProyectoEquipoVerde/ProyectoEquipoVerde/Register.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ProyectoEquipoVerde
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.ptrLogo2 = new System.Windows.Forms.PictureBox();
+            this.txtNuevaContrasenya = new System.Windows.Forms.TextBox();
+            this.txtNuevoUsuario = new System.Windows.Forms.TextBox();
+            this.lblNuevaContrasenya = new System.Windows.Forms.Label();
+            this.lblNuevoUsuario = new System.Windows.Forms.Label();
+            this.txtRepetirContrasenya = new System.Windows.Forms.TextBox();
+            this.lblRepetirContrasenya = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.btnDarseDeAlta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrLogo2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptrLogo2
+            // 
+            this.ptrLogo2.Image = ((System.Drawing.Image)(resources.GetObject("ptrLogo2.Image")));
+            this.ptrLogo2.Location = new System.Drawing.Point(113, 12);
+            this.ptrLogo2.Name = "ptrLogo2";
+            this.ptrLogo2.Size = new System.Drawing.Size(177, 155);
+            this.ptrLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptrLogo2.TabIndex = 1;
+            this.ptrLogo2.TabStop = false;
+            // 
+            // txtNuevaContrasenya
+            // 
+            this.txtNuevaContrasenya.Location = new System.Drawing.Point(129, 235);
+            this.txtNuevaContrasenya.Name = "txtNuevaContrasenya";
+            this.txtNuevaContrasenya.Size = new System.Drawing.Size(161, 20);
+            this.txtNuevaContrasenya.TabIndex = 8;
+            // 
+            // txtNuevoUsuario
+            // 
+            this.txtNuevoUsuario.Location = new System.Drawing.Point(129, 202);
+            this.txtNuevoUsuario.Name = "txtNuevoUsuario";
+            this.txtNuevoUsuario.Size = new System.Drawing.Size(161, 20);
+            this.txtNuevoUsuario.TabIndex = 7;
+            // 
+            // lblNuevaContrasenya
+            // 
+            this.lblNuevaContrasenya.AutoSize = true;
+            this.lblNuevaContrasenya.Location = new System.Drawing.Point(59, 238);
+            this.lblNuevaContrasenya.Name = "lblNuevaContrasenya";
+            this.lblNuevaContrasenya.Size = new System.Drawing.Size(64, 13);
+            this.lblNuevaContrasenya.TabIndex = 6;
+            this.lblNuevaContrasenya.Text = "Contraseña:";
+            // 
+            // lblNuevoUsuario
+            // 
+            this.lblNuevoUsuario.AutoSize = true;
+            this.lblNuevoUsuario.Location = new System.Drawing.Point(77, 205);
+            this.lblNuevoUsuario.Name = "lblNuevoUsuario";
+            this.lblNuevoUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblNuevoUsuario.TabIndex = 5;
+            this.lblNuevoUsuario.Text = "Usuario:";
+            // 
+            // txtRepetirContrasenya
+            // 
+            this.txtRepetirContrasenya.Location = new System.Drawing.Point(129, 270);
+            this.txtRepetirContrasenya.Name = "txtRepetirContrasenya";
+            this.txtRepetirContrasenya.Size = new System.Drawing.Size(161, 20);
+            this.txtRepetirContrasenya.TabIndex = 10;
+            // 
+            // lblRepetirContrasenya
+            // 
+            this.lblRepetirContrasenya.AutoSize = true;
+            this.lblRepetirContrasenya.Location = new System.Drawing.Point(22, 273);
+            this.lblRepetirContrasenya.Name = "lblRepetirContrasenya";
+            this.lblRepetirContrasenya.Size = new System.Drawing.Size(101, 13);
+            this.lblRepetirContrasenya.TabIndex = 9;
+            this.lblRepetirContrasenya.Text = "Repetir Contraseña:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 305);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(85, 308);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.lblEmail.TabIndex = 11;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // btnDarseDeAlta
+            // 
+            this.btnDarseDeAlta.Location = new System.Drawing.Point(80, 350);
+            this.btnDarseDeAlta.Name = "btnDarseDeAlta";
+            this.btnDarseDeAlta.Size = new System.Drawing.Size(229, 67);
+            this.btnDarseDeAlta.TabIndex = 13;
+            this.btnDarseDeAlta.Text = "DARSE DE ALTA";
+            this.btnDarseDeAlta.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(189)))), ((int)(((byte)(89)))));
+            this.ClientSize = new System.Drawing.Size(389, 450);
+            this.Controls.Add(this.btnDarseDeAlta);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtRepetirContrasenya);
+            this.Controls.Add(this.lblRepetirContrasenya);
+            this.Controls.Add(this.txtNuevaContrasenya);
+            this.Controls.Add(this.txtNuevoUsuario);
+            this.Controls.Add(this.lblNuevaContrasenya);
+            this.Controls.Add(this.lblNuevoUsuario);
+            this.Controls.Add(this.ptrLogo2);
+            this.Name = "Register";
+            this.Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)(this.ptrLogo2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptrLogo2;
+        private System.Windows.Forms.TextBox txtNuevaContrasenya;
+        private System.Windows.Forms.TextBox txtNuevoUsuario;
+        private System.Windows.Forms.Label lblNuevaContrasenya;
+        private System.Windows.Forms.Label lblNuevoUsuario;
+        private System.Windows.Forms.TextBox txtRepetirContrasenya;
+        private System.Windows.Forms.Label lblRepetirContrasenya;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnDarseDeAlta;
+    }
+}
